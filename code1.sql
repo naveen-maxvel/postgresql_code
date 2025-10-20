10f75ae6e4d75525559b4e9e0bf0735bd6c1201a
naveen-maxvel <naveen.maxvel1@gmail.com>
2025-10-20 23:57:44 +0530

    Update code1.sql

--- code1.sql
+++ code1.sql
@@ -96,8 +96,60 @@ GROUP BY d.department_name
 ORDER BY distinct_jobs DESC;
 
 
+SELECT * FROM employees;
+create view emp as SELECT * FROM employees;
+
+SELECT * FROM emp;
+
+--creating column month, week, year
+SELECT employee_id,
+       first_name, 
+	   last_name,
+	   salary,
+	   hire_date,
+	   TO_CHAR(hire_date, 'Mon') AS month,
+	   TO_CHAR(hire_date, 'ww') AS week_num,
+	   'week ' || TO_CHAR(hire_date, 'w') AS week_of_month,
+	   TO_CHAR(hire_date, 'yyyy') AS year
+FROM employees
+ORDER BY hire_date;
+
+--comparison of the particular month
+SELECT e.first_name || ' ' || e.last_name AS employee_full_name,
+       TO_CHAR(e.hire_date, 'Mon') AS month,
+	   SUM(CASE WHEN EXTRACT(year FROM e.hire_date) = EXTRACT(YEAR FROM current_date) - 2 THEN e.salary ELSE 0 END) AS y_2023,
+	   SUM(CASE WHEN EXTRACT(YEAR FROM e.hire_date) = EXTRACT(YEAR FROM CURRENT_DATE) -1 THEN e.salary ELSE 0 END) AS y_2024,
+	   SUM(CASE WHEN EXTRACT(YEAR FROM e.hire_date) = EXTRACT(YEAR FROM CURRENT_DATE) THEN e.salary ELSE 0 END) AS y_2025
+FROM employees e
+WHERE EXTRACT (YEAR FROM e.hire_date) >= EXTRACT(YEAR FROM CURRENT_DATE) -2 
+GROUP BY employee_full_name, month
+ORDER BY y_2023 DESC, y_2024 DESC, y_2025 DESC;
+
+
+-- comparison over years
+SELECT 
+    e.first_name || ' ' || e.last_name AS employee_full_name,
+	TO_CHAR(e.hire_date, 'Mon') AS month,
+	EXTRACT(YEAR FROM e.hire_date) AS hire_year,
+	SUM(e.salary) AS total_salary
+FROM employees e
+GROUP BY employee_full_name, month, hire_year
+ORDER BY total_salary;
+
+
+-- need the year of 2008 second quarter top 3  total salary holder for the employees 
+SELECT 
+    e.first_name || ' ' || e.last_name AS employee_full_name,
+	SUM(e.salary) AS total_salary
+FROM employees e
+WHERE EXTRACT(YEAR FROM hire_date) = 2008
+ AND  EXTRACT(QUARTER FROM hire_date) = 2
+GROUP BY employee_full_name
+ORDER BY total_salary DESC;
+
 
 	  
+
 
 
 
